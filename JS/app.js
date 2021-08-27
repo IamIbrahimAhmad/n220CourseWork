eae44eea4e1f63f3d823cbe539270edfe085f124
Ibrahim Ahmad <iahmad2@ivytech.edu>
2021-08-27 00:22:51 -0400

    seee

--- JS/app.js
+++ JS/app.js
@@ -4,5 +4,11 @@ function setup() {
 
 function draw() {
     background(220);
+    fill("blue");
+    square(50, 50, 200);
+    fill("red");
     circle(100, 100, 100);
+    circle(200, 200, 100);
+    circle(100, 200, 100);
+    circle(200, 100, 100);
 }
