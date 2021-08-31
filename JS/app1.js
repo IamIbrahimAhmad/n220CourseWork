41677ea9f5540aef06715bb8d7cc0539a6370332
Ibrahim Ahmad <iahmad2@ivytech.edu>
2021-08-30 23:03:16 -0400

    Final Lab 1

--- JS/app1.js
+++ JS/app1.js
@@ -6,9 +6,18 @@ function draw() {
     background(220);
     fill("blue");
     square(50, 50, 200);
+    fill("black");
+    circle(150, 150, 150);
+    fill("Blue");
+    circle(150, 150, 100);
     fill("red");
     circle(100, 100, 100);
+    fill("white");
     circle(200, 200, 100);
+    fill("orange");
     circle(100, 200, 100);
+    fill("pink");
     circle(200, 100, 100);
+    fill("black");
+    circle(150, 150, 50);
 }
