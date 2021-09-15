7930ffd9053044309fcda3572afd599a182c4dda
Ibrahim Ahmad <iahmad2@ivytech.edu>
2021-09-14 22:54:06 -0400

    Update app4p3.js

---
+++ Lab4/JS/app4p3.js
@@ -0,0 +1,33 @@
+function setup() {
+    createCanvas(1000, 1000);
+    frameRate(5);
+}
+
+let locationY = [];
+let locationX = [];
+
+let r;
+let g;
+let b;
+
+function draw() {
+    background(50);
+    noStroke();
+
+    for (var i = 0; i < locationY.length; i++) {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+
+        fill(r, g, b);
+        rect(locationX[i], locationY[i], 50, 25);
+
+        locationY[i] += 1;
+    }
+}
+
+function mousePressed() {
+    locationY.push(mouseY);
+    locationX.push(mouseX);
+    rect(mouseX, mouseY, 50, 25);
+}
