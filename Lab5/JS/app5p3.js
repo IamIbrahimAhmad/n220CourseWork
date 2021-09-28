c89514f10d6912c7afed9e918ac5f91038ccdecc
Ibrahim Ahmad <iahmad2@ivytech.edu>
2021-09-27 23:42:55 -0400

    5

---
+++ Lab5/JS/app5p3.js
@@ -0,0 +1,14 @@
+let div = document.getElementById("square");
+
+div.style.height = "100px";
+div.style.width = "100px";
+div.style.backgroundColor = "blue";
+document.body.style.backgroundColor = "gold";
+
+function changeBlack() {
+    div.style.backgroundColor = "black";
+}
+
+function changeBlue() {
+    div.style.backgroundColor = "blue";
+}
