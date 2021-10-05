44b0d19c1b4aa0afce76956ac3935d141c9cf7b2
Ibrahim Ahmad <iahmad2@ivytech.edu>
2021-10-04 22:43:56 -0400

    6

--- Lab6/JS/app5p1p3.js
+++ Lab6/JS/app5p1p3.js
@@ -16,8 +16,13 @@ let moves = 2;
 let playerTurn = false;
 let enemyTurn = false;
 
+let playerDef = 0;
+let enemyDef = 0;
+
 document.getElementById("hpPlayer").innerHTML = "Player HP: " + player.hp;
+document.getElementById("defPlayer").innerHTML = "Player Defense: " + playerDef;
 document.getElementById("hpEnemy").innerHTML = "Enemy HP: " + enemy.hp;
+document.getElementById("defEnemy").innerHTML = "Enemy Defense: " + enemyDef;
 document.getElementById("moves").innerHTML = moves;
 
 if (turn === 1) {
@@ -26,4 +31,91 @@ if (turn === 1) {
 } else if (turn === 2) {
     playerTurn = false;
     enemyTurn = true;
+}
+
+function playerAttack() {
+    if (playerTurn && !enemyTurn && moves > 0) {
+        enemy.hp = enemy.hp - (player.attack - enemyDef);
+        if (enemyDef > 0) {
+            enemyDef = 0;
+            document.getElementById("defEnemy").innerHTML = "Enemy Defense: " + enemyDef;
+        }
+        document.getElementById("hpEnemy").innerHTML = "Enemy HP: " + enemy.hp;
+        moves--;
+        document.getElementById("moves").innerHTML = moves;
+        if (enemy.hp <= 0) {
+            playerTurn = false;
+            enemyTurn = false;
+        }
+        if (moves === 0) {
+            playerTurn = !playerTurn;
+            enemyTurn = !enemyTurn;
+            moves = 2;
+            document.getElementById("moves").innerHTML = moves;
+        }
+    } else if (!playerTurn && enemyTurn && moves > 0) {
+        enemyT();
+    }
+}
+
+function playerDefense() {
+    if (playerTurn && !enemyTurn && moves > 0) {
+        playerDef = playerDef + player.defense;
+        document.getElementById("defPlayer").innerHTML = "Player Defense: " + playerDef;
+        moves--;
+        document.getElementById("moves").innerHTML = moves;
+        if (moves === 0) {
+            playerTurn = !playerTurn;
+            enemyTurn = !enemyTurn;
+            moves = 2;
+            document.getElementById("moves").innerHTML = moves;
+        }
+    } else if (!playerTurn && enemyTurn && moves > 0) {
+        enemyT();
+    }
+}
+
+if (!playerTurn && enemyTurn && moves > 0) {
+    enemyT();
+}
+
+function enemyT() {
+    for (let i = 0; i < 2; i++) {
+        var enemyOp = Math.floor(Math.random() * 2) + 1;
+        if (enemyOp === 1) {
+            if (!playerTurn && enemyTurn && moves > 0) {
+                player.hp = player.hp - (enemy.attack - playerDef);
+                if (playerDef > 0) {
+                    playerDef = 0;
+                    document.getElementById("defPlayer").innerHTML = "Player Defense: " + playerDef;
+                }
+                document.getElementById("hpPlayer").innerHTML = "Player HP: " + player.hp;
+                moves--;
+                document.getElementById("moves").innerHTML = moves;
+                if (enemy.hp <= 0) {
+                    playerTurn = false;
+                    enemyTurn = false;
+                }
+                if (moves === 0) {
+                    playerTurn = !playerTurn;
+                    enemyTurn = !enemyTurn;
+                    moves = 2;
+                    document.getElementById("moves").innerHTML = moves;
+                }
+            }
+        } else if (enemyOp === 2) {
+            if (!playerTurn && enemyTurn && moves > 0) {
+                enemyDef = enemyDef + enemy.defense;
+                document.getElementById("defEnemy").innerHTML = "Enemy Defense: " + enemyDef;
+                moves--;
+                document.getElementById("moves").innerHTML = moves;
+                if (moves === 0) {
+                    playerTurn = !playerTurn;
+                    enemyTurn = !enemyTurn;
+                    moves = 2;
+                    document.getElementById("moves").innerHTML = moves;
+                }
+            }
+        }
+    }
 }
