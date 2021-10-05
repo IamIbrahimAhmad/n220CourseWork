b23051f1d95694fab59c8bde5bf54205928ee6fd
Ibrahim Ahmad <iahmad2@ivytech.edu>
2021-10-04 23:54:14 -0400

    ii

--- Lab6/JS/app5p1p1.js
+++ Lab6/JS/app5p1p1.js
@@ -1,5 +1,13 @@
-let moive = {
+let moive = [{
     Name: "Justice League Snyder cut",
     Year: 2021,
-    Time: "4 hours and 5 minutes"
-}
+    Time: "4 hours and 2 minutes"
+}, {
+    Name: "Man of Steel",
+    Year: 2013,
+    Time: "2 hours and 23 minutes"
+}, {
+    Name: "Batman V Superman: Dawn Of Justice",
+    Year: 2016,
+    Time: "2 hours and 31 minutes"
+}]
