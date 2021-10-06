b63f781d0890e6b429f130cf3c2ad3ac94773e16
Ibrahim Ahmad <iahmad2@ivytech.edu>
2021-10-05 22:40:21 -0400

    77

--- Lab6/JS/app5p1p1.js
+++ Lab6/JS/app5p1p1.js
@@ -1,4 +1,4 @@
-let moive = [{
+let movie = [{
     Name: "Justice League Snyder cut",
     Year: 2021,
     Time: "4 hours and 2 minutes"
@@ -11,3 +11,17 @@ let moive = [{
     Year: 2016,
     Time: "2 hours and 31 minutes"
 }]
+
+document.body.style.backgroundColor = "gold";
+
+for (let i = 0; i <= 2; i++) {
+    document.getElementById("movie" + i).style.color = "blue";
+    document.getElementById("movie" + i).style.fontSize = "45px";
+    document.getElementById("movie" + i).style.fontFamily = "Brush Script MT, cursive";
+    document.getElementById("movie" + i).innerHTML = "Movie Name: " + movie[i].Name + ", Year: " + movie[i].Year.toString() + ", Time: " + movie[i].Time;
+    if (i === 1) {
+        document.getElementById("movie" + i).style.color = "red";
+        document.getElementById("movie" + i).style.fontSize = "35px";
+        document.getElementById("movie" + i).style.fontFamily = "OCR A Std, monospace";
+    }
+}
