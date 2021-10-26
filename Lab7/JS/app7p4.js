77376d74786a1247715e9c554e436c30b066367d
Ibrahim Ahmad <iahmad2@ivytech.edu>
2021-10-25 22:32:13 -0400

    k

--- Lab7/JS/app7p4.js
+++ Lab7/JS/app7p4.js
@@ -4,8 +4,8 @@ let x2 = document.getElementById("x2");
 let y2 = document.getElementById("y2");
 
 function Calculate() {
-    let xDiffs = x2.value - x1.value;
-    let yDiffs = y2.value - y1.value;
+    let xDiffs = parseFloat(x2.value) - parseFloat(x1.value);
+    let yDiffs = parseFloat(y2.value) - parseFloat(y1.value);
     let distance = Math.sqrt(xDiffs * xDiffs + yDiffs * yDiffs);
     document.getElementById("h1").innerHTML = "The distance between the point (" + x1.value + ", " + y1.value + ")" + " and the point (" + x2.value + ", " + y2.value + ") is: " + distance + ".";
     console.log("The distance between the point (" + x1.value + ", " + y1.value + ")" + " and the point (" + x2.value + ", " + y2.value + ") is: " + distance + ".");
