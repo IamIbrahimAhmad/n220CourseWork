c6c801178e657cb90cdefc258d7f681e2e389161
Ibrahim Ahmad <iahmad2@ivytech.edu>
2021-10-27 00:39:33 -0400

    week10

--- Lab7/JS/app7p5.js
+++ Lab7/JS/app7p5.js
@@ -1,35 +1,17 @@
-function setup() {
-    createCanvas(800, 600);
-}
+let rNum = 10;
+let h1 = document.getElementById("h1");
 
-let x = 150;
-let y = 150;
-
-let speedX = 6;
-let speedY = 2;
-
-let size = 25;
-
-let r = 0;
-let g = 0;
-let b = 155;
-
-function draw() {
-    background(200);
-    fill(r, g, b)
-    circle(x, y, size * 2);
-    x += speedX;
-    y += speedY;
-    if (x > (width - size) || x < size) {
-        speedX = -speedX;
-        r = random(255);
-        g = random(255);
-        b = random(255);
-    }
-    if (y > (height - size) || y < size) {
-        speedY = -speedY;
-        r = random(255);
-        g = random(255);
-        b = random(255);
+function check() {
+    let num = parseInt(document.getElementById("num").value);
+    if (num > 20 || num <= 0) {
+        return h1.innerHTML = "Please enter a number between 1 and 20.";
+    } else if (num === rNum) {
+        return h1.innerHTML = "Right.";
+    } else if (num !== rNum) {
+        if (num > rNum) {
+            return h1.innerHTML = "Wrong, you rnumber is greater. Guess again.";
+        } else if (num < rNum) {
+            return h1.innerHTML = "Wrong, you rnumber is smaller. Guess again.";
+        }
     }
 }
---
+++ Lab7/JS/app7p6.js
@@ -0,0 +1,17 @@
+let rNum = Math.round(Math.random() * 20);
+let h1 = document.getElementById("h1");
+
+function check() {
+    let num = parseInt(document.getElementById("num").value);
+    if (num > 20 || num <= 0) {
+        return h1.innerHTML = "Please enter a number between 1 and 20.";
+    } else if (num === rNum) {
+        return h1.innerHTML = "Right.";
+    } else if (num !== rNum) {
+        if (num > rNum) {
+            return h1.innerHTML = "Wrong, you rnumber is greater. Guess again.";
+        } else if (num < rNum) {
+            return h1.innerHTML = "Wrong, you rnumber is smaller. Guess again.";
+        }
+    }
+}
